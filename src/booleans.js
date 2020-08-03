6ebee1054cc605c16a13c0b753b7ee35e238b453
Billy Morrison <billy.morrison1234@hotmail.co.uk>
2020-08-03 23:03:43 +0100

    made progress on booleans

--- src/booleans.js
+++ src/booleans.js
@@ -1,57 +1,46 @@
-const negate = a => {
-  // your code here
-};
+const negate = a => !a;
 
-const both = (a, b) => {
-  // your code here
-};
+const both = (a, b) => a && b;
 
-const either = (a, b) => {
-  // your code here
-};
+const either = (a, b) => a || b;
 
-const none = (a, b) => {
-  // your code here
-};
+//come back
+const none = (a, b) => !a && !b;
 
+//come back
 const one = (a, b) => {
   // your code here
 };
 
-const truthiness = a => {
-  // your code here
-};
+const truthiness = a => Boolean(a);
 
-const isEqual = (a, b) => {
-  // your code here
-};
+const isEqual = (a, b) => a === b;
 
-const isGreaterThan = (a, b) => {
-  // your code here
-};
+const isGreaterThan = (a, b) => a > b;
 
-const isLessThanOrEqualTo = (a, b) => {
-  // your code here
-};
+const isLessThanOrEqualTo = (a, b) => a <= b;
 
-const isOdd = a => {
-  // your code here
-};
+const isOdd = a => a % 2 !== 0;
 
-const isEven = a => {
-  // your code here
-};
+const isEven = a => a % 2 === 0;
 
-const isSquare = a => {
-  // your code here
-};
+const isSquare = a => Math.sqrt(a) === Math.floor(Math.sqrt(a));
 
 const startsWith = (char, string) => {
   // your code here
 };
 
 const containsVowels = string => {
-  // your code here
+  const newString = string.toLowerCase();
+  const vowels = ["a", "e", "i", "o", "u"];
+  for (let i = 0; i < vowels.length; i++) {
+    for (let j = 0; j < newString.length; j++) {
+      if (vowels[i] === newString[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 const isLowerCase = string => {
